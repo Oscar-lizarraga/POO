ecdfbb136c9c52f120f2675ce61946cfff4f1a07
RodrigoBau2507 <91805080+RodrigoBau2507@users.noreply.github.com>
2021-11-06 23:04:50 -0600

    Update Boleto.java

--- Boleto.java
+++ Boleto.java
@@ -4,14 +4,11 @@ public class Boleto{
 	private int idBoleto;
 	private Date fechaVenta;
 	private Funcion funcion;
-	private Sala sala;
-
 	
 	public Boleto(int idBoleto, Date fechaVenta, Funcion funcion, Sala sala /*Double precioTotal*/) {
 		this.idBoleto = idBoleto;
 		this.fechaVenta = fechaVenta;
 		this.funcion = funcion;
-		this.sala = sala;
 	}
 	
 	public int getIdBoleto() {
@@ -34,11 +31,5 @@ public class Boleto{
 	public void setFuncion(Funcion funcion){
 		this.funcion = funcion;
 	}
-
-	public Sala getSala() {
-		return sala;
-	}
-	public void setSala(Sala sala){
-		this.sala = sala;
-	}	
+	
 }
