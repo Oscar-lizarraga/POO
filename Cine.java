10909d2a4c255052191848ac01273b703065848f
MoisesNav <93019359+MoisesNav@users.noreply.github.com>
2021-11-09 15:43:27 -0600

    Add files via upload

---
+++ Cine.java
@@ -0,0 +1,29 @@
+
+package proyectocine;
+
+
+public class Cine {
+    private String direccion;
+    private String nombreCine;
+    
+    
+    
+    
+    
+    
+    
+    //getters and seetters
+    public String getDireccion() {
+        return direccion;
+    }
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+    public String getNombreCine() {
+        return nombreCine;
+    }
+    public void setNombreCine(String nombreCine) {
+        this.nombreCine = nombreCine;
+    }
+    
+}
